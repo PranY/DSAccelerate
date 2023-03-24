d17479d5a4fa78a5fee4f3ac3b421d3003443569
PranY <pranjal.yadav@razorpay.com>
2023-03-24 16:52:28 +0530

    Merging specifications and cookiecutter config into project config

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import subprocess
 subprocess.run(["pip", "install", "cookiecutter"])
 
 # Set the path to the custom JSON file
-cookiecutter_json_path = Path.cwd()/'cookiecutter.json'
+cookiecutter_json_path = Path.cwd()/'project_config.json'
 # Call the cookiecutter command-line tool with the custom JSON file
 subprocess.call(["cookiecutter", "https://github.com/drivendata/cookiecutter-data-science","--no-input","--config-file", str(cookiecutter_json_path), "-v", ])
