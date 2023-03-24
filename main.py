c8e145ae279f088a9ae53f46f8f49ca6ea6d5263
PranY <pranjal.yadav@razorpay.com>
2023-03-24 16:45:16 +0530

    Fixed project name issue with cookiecutter.json

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-import os
+from pathlib import Path
 import subprocess
 
-# Specify the project parameters
-project_name = "my_project"
-repo_name = project_name
-author_name = "Razorpay"
-description = "A default description."
-license = "3 - No license file"
+# Install Cookiecutter
+subprocess.run(["pip", "install", "cookiecutter"])
 
-# Check if the project directory already exists
-if os.path.exists(project_name):
-    print(f"Project directory '{project_name}' already exists.")
-else:
-    # Install Cookiecutter
-    subprocess.run(["pip", "install", "cookiecutter"])
-
-    # Create a new project using Cookiecutter
-    cookiecutter_params = {
-        "project_name": project_name,
-        "repo_name": repo_name,
-        "author_name": author_name,
-        "description": description,
-        "open_source_license": license,
-        "aws_s3_bucket": "no",
-        "python_interpreter": "python3"
-    }
-    subprocess.run(["cookiecutter", "https://github.com/drivendata/cookiecutter-data-science.git", "--no-input"], input=str(cookiecutter_params).encode())
-    print(f"Created new project '{project_name}' using Cookiecutter.")
+# Set the path to the custom JSON file
+cookiecutter_json_path = Path.cwd()/'cookiecutter.json'
+# Call the cookiecutter command-line tool with the custom JSON file
+subprocess.call(["cookiecutter", "https://github.com/drivendata/cookiecutter-data-science","--no-input","--config-file", str(cookiecutter_json_path), "-v", ])
